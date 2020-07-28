65f1652549f8d3acf3027d0a08874411996ac745
Scott Havens <scott.havens@ars.usda.gov>
2020-07-28 14:16:47 -0600

    more explict in the original snobal call and now it matches to within an acceptible error

--- pysnobal/tests/test_data_point/gold_ipw/run_snobal.sh
+++ pysnobal/tests/test_data_point/gold_ipw/run_snobal.sh
@@ -1,5 +1,5 @@
 # normal output
-snobal -z 2061 -t 60 -m 0.01 -d 0.25 -s gold.snow.properties.input -h gold.inheight.input -p gold.snobal.ppt.input -i gold.snobal.data.input.short -O data -o gold.snobal.out -c
+snobal -z 2061 -m 0.01 -d 0.25 -t 60,60,15,1 -T 60,10,1 -s gold.snow.properties.input -h gold.inheight.input -p gold.snobal.ppt.input -i gold.snobal.data.input.short -O data -o gold.snobal.out -c
 
 # all output
-snobal -z 2061 -t 60 -m 0.01 -d 0.25 -s gold.snow.properties.input -h gold.inheight.input -p gold.snobal.ppt.input -i gold.snobal.data.input.short -O data -o gold.snobal.out.all -c -O all
+snobal -z 2061 -m 0.01 -d 0.25 -t 60,60,15,1 -T 60,10,1 -s gold.snow.properties.input -h gold.inheight.input -p gold.snobal.ppt.input -i gold.snobal.data.input.short -O data -o gold.snobal.out.all -c -O all
